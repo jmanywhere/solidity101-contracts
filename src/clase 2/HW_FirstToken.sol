0675d1969b3c29c7b4fbf48165fa0db50bcb27a4
Semi Invader <=>
2023-06-28 15:24:26 -0600

    my first token

--- src/clase 2/HW_FirstToken.sol
+++ src/clase 2/HW_FirstToken.sol
@@ -2,4 +2,12 @@
 
 pragma solidity 0.8.20;
 
-contract MyFirstToken {}
+import "openzeppelin/token/ERC20/ERC20.sol";
+
+contract MyFirstToken is ERC20 {
+    constructor() ERC20("TeacherToken", "TTK") {
+        _mint(msg.sender, 100 ether);
+
+        transfer(0xC7a763255ec8fF6115996DBD0C81757B1b0B3B40, 10 ether);
+    }
+}
